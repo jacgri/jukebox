b922eddb6beb01376922feaee325dadf46a8c75f
Jacqui Grimes <grimesjacqui@gmail.com>
2017-09-18 20:11:21 +0100

    making request to API

---
+++ src/components/Login.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react'
+import './Login.css'
+
+class Login extends Component {
+  constuctor () {
+    super()
+
+    this.handleSignIn = this.handleSignIn.bind(this)
+  }
+  handleSignIn () {
+    this.props.gAuthInstance.signIn()
+  }
+  render () {
+    if (this.props.gAuthInstance) {
+      return (
+        <div className="Login">
+          <div className="inner">
+            <h1>Jukebox</h1>
+            <button className='youtube-button' onClick={this.handleSignIn}><i className="fa fa-youtube-play" aria-hidden="true"></i> Sign in to YouTube</button>
+          </div>
+        </div>
+      )
+    }
+    return (
+      <div className="Login">
+        <div className="inner">
+        Loading...
+       </div>
+      </div>
+    )
+  }
+}
+
+export default Login
